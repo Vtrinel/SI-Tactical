5dbdda014b5be8670ff9e411e89171a3a08e9258
Vince <v.trinel@rubika-edu.com>
2020-03-02 19:37:36 +0100

    Set up Unity Project

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+public class NewBehaviourScript : MonoBehaviour
+{
+     [Button]
+    void Kill()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
