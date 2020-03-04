463cde2d923abb40b8f2c1e4ea505fabfbb8afb5
Drusaks <david.durand.253@gmail.com>
2020-03-04 17:54:10 +0100

    Commit obligatoire

---
+++ Assets/Scripts/Competences/CompetenceUnlockDisplay.cs
@@ -0,0 +1,38 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class CompetenceUnlockDisplay : MonoBehaviour
+{
+
+    public TextMeshProUGUI description;
+    public TextMeshProUGUI ActionPoint;
+    public TextMeshProUGUI UnlockCost;
+
+    private Competence competence;
+
+    private void OnEnable()
+    {
+        PlayerExperienceManager._instance.OnSelectCompetence += RefreshCompetence;
+    }
+
+    private void OnDisable()
+    {
+        PlayerExperienceManager._instance.OnSelectCompetence -= RefreshCompetence;
+    }
+
+    // Change the UI for the competence info
+    private void RefreshCompetence()
+    {
+        competence = PlayerExperienceManager._instance.GetSelectedCompetence();
+
+        description.text = competence.Getdescription;
+        ActionPoint.text = competence.GetActionPointsCost.ToString();
+        UnlockCost.text = competence.GetPointsCost.ToString();
+    }
+
+    // Send the request to unlock the competence
+    public void unlockCompetence()
+    {
+        PlayerExperienceManager._instance.CanUnlockCompetence(competence);
+    }
+}
---
+++ Assets/Scripts/Manager/PlayerExperienceManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerExperienceManager : MonoBehaviour
+{
+    // Public attributes
+    public static PlayerExperienceManager _instance;
+
+    [Header("Recall Competences")]
+    public List<CompetenceRecall> RecallCompetences = new List<CompetenceRecall>();
+
+    [Header("Throw Competences")]
+    public List<CompetenceThrow> ThrowCompetence = new List<CompetenceThrow>();
+
+    //Actions
+    public event Action OnSelectCompetence;
+    public event Action<Competence> OnTryUnlockCompetence;
+
+
+    // Private Attributes
+    private int competencesPoints = 0;
+    private Competence selectedCompetence;
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public static PlayerExperienceManager Instance { get { return _instance; } }
+
+
+    // Check if a competence can be unlocked and unlock it if possible
+    public void CanUnlockCompetence(Competence competence)
+    {
+        OnTryUnlockCompetence?.Invoke(competence);
+
+        Debug.Log("Competences points : " + competencesPoints);
+
+        if (competence.GetPointsCost <= competencesPoints)
+        {
+            competence.SetUnlockedState(true);
+            competencesPoints -= competence.GetPointsCost;
+
+            Debug.Log("Unlocked");
+
+        }
+
+        Debug.Log("Competences points : " + competencesPoints);
+    }
+
+    // Setter SelectedCompetence
+    public void SelectCompetence(Competence competence)
+    {
+        OnSelectCompetence?.Invoke();
+
+        selectedCompetence = competence;
+    }
+    
+    // Getter SelectedCompetence
+    public Competence GetSelectedCompetence()
+    {
+        return selectedCompetence;
+    }
+}
