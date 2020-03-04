a8dea5cf998b1557a5072f297eed7e38ba17212e
Kelda <sebastien.butor@orange.fr>
2020-03-04 16:11:22 +0100

    Damage System Bases

---
+++ Assets/Scripts/DamageSystem/LifeSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LifeSystem
+{
+    [SerializeField] int maxLifeAmount = 10;
+    int currentLifeAmount;
+    public int GetCurrentLifeAmount => currentLifeAmount;
+
+    /// <summary>
+    /// First parameter is current life, second parameter is life differential
+    /// </summary>
+    public Action<int, int> OnCurrentLifeAmountChanged;
+    public Action OnLifeReachedZero;
+
+    public void SetUpSystem()
+    {
+        ResetLifeAmount();
+    }
+
+    public void ResetLifeAmount()
+    {
+        currentLifeAmount = maxLifeAmount;
+
+        OnCurrentLifeAmountChanged?.Invoke(currentLifeAmount, 0);
+    }
+
+    public void LoseLife(int amount)
+    {
+        currentLifeAmount = Mathf.Clamp(currentLifeAmount - Mathf.Abs(amount), 0, maxLifeAmount);
+        OnCurrentLifeAmountChanged?.Invoke(currentLifeAmount, -Mathf.Abs(amount));
+
+        if (currentLifeAmount == 0)
+            OnLifeReachedZero?.Invoke();
+    }
+
+    public void RegainLife(int amount)
+    {
+        currentLifeAmount = Mathf.Clamp(currentLifeAmount + Mathf.Abs(amount), 0, maxLifeAmount);
+        OnCurrentLifeAmountChanged?.Invoke(currentLifeAmount, Mathf.Abs(amount));
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    private void Start()
+    {
+        damageReceiptionSystem.SetUpSystem();
+        damageReceiptionSystem.OnCurrentLifeAmountChanged += DebugLifeChange;
+        damageReceiptionSystem.OnLifeReachedZero += Die;
+    }
+
+    [Header("References")]
+    [SerializeField] DamageableEntity damageReceiptionSystem = default;
+
+    public void Die()
+    {
+        Debug.Log(name + " (Enemy) is dead");
+    }
+    
+    public void DebugLifeChange(int currentAmount, int delta)
+    {
+        Debug.Log((delta < 0 ? "Lost" : "Regained") + delta + "HP. Current life if " + currentAmount + "HP");
+    }
+}
