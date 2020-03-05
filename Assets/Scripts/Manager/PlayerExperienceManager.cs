33a0966c58e5060b96a6bdcfb08bfe7250650c8a
Drusaks <david.durand.253@gmail.com>
2020-03-05 17:18:57 +0100

    Arbre de compétence fonctionnel => manque lien avec le joueur

--- Assets/Scripts/Manager/PlayerExperienceManager.cs
+++ Assets/Scripts/Manager/PlayerExperienceManager.cs
@@ -9,9 +9,7 @@ public class PlayerExperienceManager : MonoBehaviour
     // Public attributes
     public static PlayerExperienceManager _instance;
 
-    [Header("Lists competences")]
-    public List<CompetenceRecall> RecallCompetences = new List<CompetenceRecall>();
-    public List<CompetenceThrow> ThrowCompetence = new List<CompetenceThrow>();
+    public Canvas competenceCanvas;
 
     //Actions
     public event Action<int> OnSelectTypeCompetence;
@@ -21,12 +19,27 @@ public class PlayerExperienceManager : MonoBehaviour
 
 
     // Private Attributes
-    private int competencesPoints = 2;
+    
     private Competence selectedCompetence;
+
     private List<Competence> listUnlockedCompetences = new List<Competence>();
     private List<Competence> listEquipedCompetences = new List<Competence>();
-    private int competenceTypeSelected; // 0 = throw, 1 = recall, 2 = special
 
+    public GameObject throwInterface;
+    public GameObject recallInterface;
+    public GameObject specialInterface;
+
+    private int competenceTypeSelected = 0; // 0 = throw, 1 = recall, 2 = special
+    private int competencesPoints = 1;
+
+    private bool isCanvasCompetenceShowed = false;
+
+    //private void Start()
+    //{
+    //    throwInterface = GameObject.Find("MenuCompetenceThrow");
+    //    recallInterface = GameObject.Find("MenuCompetenceRecall");
+    //    specialInterface = GameObject.Find("MenuCompetenceSpecial");
+    //}
 
     // For the singleton
     private void Awake()
@@ -41,6 +54,57 @@ public class PlayerExperienceManager : MonoBehaviour
         }
     }
     public static PlayerExperienceManager Instance { get { return _instance; } }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            IsCompetenceInterfaceShowing();
+        }
+
+        // Change the competence interface on click
+        switch (competenceTypeSelected)
+        {
+            case 0:
+                throwInterface.gameObject.SetActive(true);
+                recallInterface.gameObject.SetActive(false);
+                specialInterface.gameObject.SetActive(false);
+                break;
+
+            case 1:
+                throwInterface.gameObject.SetActive(false);
+                recallInterface.gameObject.SetActive(true);
+                specialInterface.gameObject.SetActive(false);
+                break;
+
+            case 2:
+                throwInterface.gameObject.SetActive(false);
+                recallInterface.gameObject.SetActive(false);
+                specialInterface.gameObject.SetActive(true);
+                break;
+
+            default:
+                throwInterface.gameObject.SetActive(true);
+                recallInterface.gameObject.SetActive(false);
+                specialInterface.gameObject.SetActive(false);
+                break;
+        }
+    }
+
+    // Show or not the competence interface
+
+    public void IsCompetenceInterfaceShowing()
+    {
+        isCanvasCompetenceShowed = !isCanvasCompetenceShowed;
+        competenceCanvas.gameObject.SetActive(isCanvasCompetenceShowed);
+    }
+
+    // Change the type of competence selected
+    public void SelectTypeCompetence(int type)
+    {
+        competenceTypeSelected = type;
+        OnSelectTypeCompetence?.Invoke(type);
+    }
 
     // Check if a competence can be unlocked and unlock it if possible
     public void CanUnlockCompetence(Competence competence)
@@ -69,13 +133,6 @@ public class PlayerExperienceManager : MonoBehaviour
         selectedCompetence = competence;
 
         OnSelectCompetence?.Invoke(competence);
-    }
-
-    // Change the type of competence selected
-    public void SelectTypeCompetence(int type)
-    {
-        competenceTypeSelected = type;
-        OnSelectTypeCompetence?.Invoke(type);
     }
 
     public void EquipCompetence(Competence competence)
@@ -155,5 +212,10 @@ public class PlayerExperienceManager : MonoBehaviour
         var type = assembly.GetType("UnityEditor.LogEntries");
         var method = type.GetMethod("Clear");
         method.Invoke(new object(), null);
+    }
+
+    public void AddCompetencePoint()
+    {
+        competencesPoints++;
     }
 }
