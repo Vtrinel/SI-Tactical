981605f2718fd3fc500e8530aa0fab74a2f6365b
Kelda <sebastien.butor@orange.fr>
2020-03-07 19:37:30 +0100

    Rappel des disques - Amélioration A FAIRE : faire de la récupération de la liste des disques récupérables une factory, et l'utiliser pour les previews

--- Assets/Scripts/Competences/CompetenceRecall.cs
+++ Assets/Scripts/Competences/CompetenceRecall.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Recall Competence", menuName = "Competence/Recall/Base")]
 public class CompetenceRecall : CompetenceDisc
 {
+    [Header("Recall Parameters")]
+    [Tooltip("0 will recall all in-range discs")] [SerializeField] int numberOfRecalledDiscs = 0;
+    public int GetNumberOfRecalledDiscs => numberOfRecalledDiscs;
 
+    [SerializeField] DiscsOrder recallingOrder = DiscsOrder.FromOldestToNewest;
+    public DiscsOrder GetRecallingOrder => recallingOrder;
+
+    [SerializeField] bool canRecallUnthrowedDiscs = true;
+    public bool GetCanRecallUnthrowedDiscs => canRecallUnthrowedDiscs;
 }
 
--- Assets/Scripts/Competences/TrajectoryModifiers/TrajectoryModifierLinkedDiscs.cs
+++ Assets/Scripts/Competences/TrajectoryModifiers/TrajectoryModifierLinkedDiscs.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Discs Link Trajectory", menuName = "Competence/Trajectory Modifiers/Discs Link")]
 public class TrajectoryModifierLinkedDiscs : TrajectoryModifier
 {
-    [SerializeField] LinkedDiscTrajectoryType linkedDiscTrajectoryType = LinkedDiscTrajectoryType.FromOldestToNewest;
-    public LinkedDiscTrajectoryType GetLinkedDiscTrajectoryType => linkedDiscTrajectoryType;
-}
-
-public enum LinkedDiscTrajectoryType
-{
-    FromOldestToNewest, FromNewestToOldest, None
+    [SerializeField] DiscsOrder linkedDiscTrajectoryType = DiscsOrder.FromOldestToNewest;
+    public DiscsOrder GetLinkedDiscTrajectoryType => linkedDiscTrajectoryType;
 }
