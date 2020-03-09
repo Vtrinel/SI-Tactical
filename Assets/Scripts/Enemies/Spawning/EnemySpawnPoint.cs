aec0f5182812d024542eb44ff203df9cf2e0bc23
Kelda <sebastien.butor@orange.fr>
2020-03-09 12:33:14 +0100

    Enemy spawning system bases

---
+++ Assets/Scripts/Enemies/Spawning/EnemySpawnPoint.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnPoint : MonoBehaviour
+{
+    /// WARNING : The current spawning system will never check if the spawning position is taken by a disc, the player or an enemy. 
+    /// This should be improved if possible
+
+    [SerializeField] EnemyType definedEnemyToSpawn = EnemyType.None;
+
+    public void StartSpawning()
+    {
+        if (definedEnemyToSpawn == EnemyType.None)
+            return;
+        StartSpawning(definedEnemyToSpawn);
+    }
+
+    EnemyType nextSpawnEnemyType = EnemyType.None;
+    public void StartSpawning(EnemyType enemyTypeToSpawn)
+    {
+        if (enemyTypeToSpawn == EnemyType.None)
+            return;
+        nextSpawnEnemyType = enemyTypeToSpawn;
+
+        /// NEXT STEP : add to spawn points to check on next player turn start + generate a preview
+        /// For now, just spawn instant
+        SpawnEnemyOnSpawnPoint(nextSpawnEnemyType);
+    }
+
+    public void SpawnEnemyOnSpawnPoint(EnemyType enemyTypeToSpawn)
+    {
+        EnemiesManager.Instance.SpawnEnemyAtPosition(enemyTypeToSpawn, transform.position);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, 0.2f);
+    }
+}
---
+++ Assets/Scripts/Enemies/Spawning/EnemySwarmZone.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySwarmZone : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        if (selfActivates)
+            GameManager.Instance.OnPlayerPositionChanged += CheckIfPlayerIsInZone;
+    }
+
+    private void OnDisable()
+    {
+        if (selfActivates)
+            GameManager.Instance.OnPlayerPositionChanged -= CheckIfPlayerIsInZone;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!debugZone)
+            return;
+
+        Gizmos.color = new Color(1, 0, 1, 0.4f);
+        Gizmos.DrawSphere(transform.position, radius);
+    }
+
+    [SerializeField] bool debugZone = true;
+
+    [Header("Parameters")]
+    [SerializeField] float radius = 5f;
+    [SerializeField] bool selfActivates = false;
+
+    SwarmZoneState currentState = SwarmZoneState.Inactive;
+
+    public void CheckIfPlayerIsInZone(Vector3 playerPos)
+    {
+        switch (currentState)
+        {
+            case SwarmZoneState.Inactive:
+                if (GetDistanceWithPlayer(playerPos) <= radius)
+                    OnPlayerEntersZone();
+                break;
+
+            case SwarmZoneState.PlayerInZone:
+                if (GetDistanceWithPlayer(playerPos) > radius)
+                    OnPlayerExitsZone();
+                break;
+
+            case SwarmZoneState.PlayerOutZoneButEnteredOnce:
+                if (GetDistanceWithPlayer(playerPos) <= radius)
+                    OnPlayerEntersZone();
+                break;
+        }
+    }
+
+    public float GetDistanceWithPlayer(Vector3 playerPos)
+    {
+        Vector3 selfPos = transform.position;
+        playerPos.y = transform.position.y;
+
+        return Vector3.Distance(playerPos, selfPos);
+    }
+
+    public void OnPlayerEntersZone()
+    {
+        if(currentState == SwarmZoneState.Inactive)
+        {
+            OnPlayerEntersZoneFirstTime();
+        }
+        currentState = SwarmZoneState.PlayerInZone;
+    }
+
+    public void OnPlayerEntersZoneFirstTime()
+    {
+
+    }
+
+    public void OnPlayerExitsZone()
+    {
+        currentState = SwarmZoneState.PlayerOutZoneButEnteredOnce;
+    }
+}
+
+public enum SwarmZoneState
+{
+    Inactive, PlayerInZone, PlayerOutZoneButEnteredOnce
+}
