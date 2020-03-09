185bfa8e281a948f54392be7e54d8de68d52143d
PLSAM <samarachtingi@gmail.com>
2020-03-09 18:06:20 +0100

    no message

--- Assets/Scripts/IA/ShowPathSystem.cs
+++ Assets/Scripts/IA/ShowPathSystem.cs
@@ -5,10 +5,39 @@ using UnityEngine.AI;
 
 public class ShowPathSystem : MonoBehaviour
 {
-    public LineRenderer lineDeplacement;
-    public LineRenderer lineCantDeplacement;
+    [SerializeField] LineRenderer lineDeplacement;
+    [SerializeField] LineRenderer lineCantDeplacement;
+    [SerializeField] NavMeshAgent myNavAgent;
+    float distance;
+    float attackRange;
 
-    public void DrawPath(NavMeshAgent myNavAgent, float _distance)
+    bool showPreview = false;
+
+    public void SetValue(float _distance, float _attackRange)
+    {
+        distance = _distance;
+        attackRange = _attackRange;
+    }
+
+    public void ShowOrHide(bool value)
+    {
+        showPreview = value;
+    }
+
+    private void Update()
+    {
+        if (showPreview)
+        {
+            DrawPath();
+        }
+        else
+        {
+            lineDeplacement.positionCount = 0;
+            lineCantDeplacement.positionCount = 0;
+        }
+    }
+
+    public void DrawPath()
     {
         myNavAgent.isStopped = true;
         myNavAgent.SetDestination(GameManager.Instance.GetCurrentWorldMouseResult.mouseWorldPosition);
@@ -23,13 +52,13 @@ public class ShowPathSystem : MonoBehaviour
         float currentDistance = 0;
         for (var i = 1; i < _path.corners.Length; i++)
         {
-            if (currentDistance > _distance)
+            if (currentDistance > distance)
             {
                 cantPos.Add(_path.corners[i]);
             }
             else
             {
-                if (currentDistance + Vector3.Distance(_path.corners[i - 1], _path.corners[i]) < _distance)
+                if (currentDistance + Vector3.Distance(_path.corners[i - 1], _path.corners[i]) < distance)
                 {
                     canPos.Add(_path.corners[i]);
                     currentDistance += Vector3.Distance(_path.corners[i - 1], _path.corners[i]);
@@ -37,11 +66,9 @@ public class ShowPathSystem : MonoBehaviour
                 else
                 {
                     //Calcule de coupure de la ligne
-                    float distanceTokeep = _distance - currentDistance;
-                    print(distanceTokeep);
+                    float distanceTokeep = distance - currentDistance;
 
                     Vector3 dir = _path.corners[i] - _path.corners[i - 1];
-
                     Vector3 targetPosition = _path.corners[i - 1] + dir.normalized * distanceTokeep;
 
                     canPos.Add(targetPosition);
