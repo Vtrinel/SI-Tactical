f3424a1f5629bb4326b84c437bd7b458d7835fe3
PLSAM <samarachtingi@gmail.com>
2020-03-11 10:48:12 +0100

    update line

--- Assets/Scripts/IA/ShowPathSystem.cs
+++ Assets/Scripts/IA/ShowPathSystem.cs
@@ -6,7 +6,6 @@ using UnityEngine.AI;
 public class ShowPathSystem : MonoBehaviour
 {
     [SerializeField] LineRenderer lineDeplacement;
-    [SerializeField] LineRenderer lineCantDeplacement;
     [SerializeField] NavMeshAgent myNavAgent;
     float distance;
     float attackRange;
@@ -39,7 +38,6 @@ public class ShowPathSystem : MonoBehaviour
         else
         {
             lineDeplacement.positionCount = 0;
-            lineCantDeplacement.positionCount = 0;
         }
     }
 
@@ -53,7 +51,6 @@ public class ShowPathSystem : MonoBehaviour
         List<Vector3> canPos = new List<Vector3>();
         canPos.Add(transform.position);
 
-        List<Vector3> attackPos = new List<Vector3>();
 
         float currentDistance = 0;
         float currentAttackDistance = 0;
@@ -62,15 +59,14 @@ public class ShowPathSystem : MonoBehaviour
         {
             if (currentDistance > distance)
             {
-                if(currentAttackDistance + Vector3.Distance(attackPos[attackPos.Count - 1], _path.corners[i]) > attackRange)
+                if(currentAttackDistance + Vector3.Distance(canPos[canPos.Count - 1], _path.corners[i]) > attackRange)
                 {
                     lineDeplacement.positionCount = 0;
-                    lineCantDeplacement.positionCount = 0;
                     return;
                 }
                 else
                 {
-                    attackPos.Add(_path.corners[i]);
+                    canPos.Add(_path.corners[i]);
                     currentAttackDistance += Vector3.Distance(_path.corners[i - 1], _path.corners[i]);
                 }
             }
@@ -91,17 +87,14 @@ public class ShowPathSystem : MonoBehaviour
 
                     canPos.Add(targetPosition);
 
-                    attackPos.Add(targetPosition);
-
                     if(Vector3.Distance(targetPosition, _path.corners[i]) > attackRange)
                     {
                         lineDeplacement.positionCount = 0;
-                        lineCantDeplacement.positionCount = 0;
                         return;
                     }
                     else
                     {
-                        attackPos.Add(_path.corners[i]);
+                        canPos.Add(_path.corners[i]);
                         currentAttackDistance += Vector3.Distance(targetPosition, _path.corners[i]);
                     }
 
@@ -109,16 +102,13 @@ public class ShowPathSystem : MonoBehaviour
                 }
             }
 
-            SetLines(canPos, attackPos);
+            SetLines(canPos);
         }
     }
 
-    void SetLines(List<Vector3> _canPos, List<Vector3> _attackPos)
+    void SetLines(List<Vector3> _canPos)
     {
         lineDeplacement.positionCount = _canPos.Count;
         lineDeplacement.SetPositions(_canPos.ToArray());
-
-        lineCantDeplacement.positionCount = _attackPos.Count;
-        lineCantDeplacement.SetPositions(_attackPos.ToArray());
     }
 }
