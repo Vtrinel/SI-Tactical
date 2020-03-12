ef1a2c2a07187d31256e0666b3f93ac2e5795903
PLSAM <samarachtingi@gmail.com>
2020-03-12 22:26:56 +0100

    trail fix + ui

--- Assets/Scripts/UI/DiscElement.cs
+++ Assets/Scripts/UI/DiscElement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,7 +10,7 @@ public class DiscElement : MonoBehaviour
     [SerializeField] List<Image> AllImages = new List<Image>();
     [SerializeField] List<Text> AllTexts = new List<Text>();
 
-    [SerializeField] Text textValue;
+    [SerializeField] TextMeshProUGUI textValue;
 
     [SerializeField] TooltipColliderUI tooltipCollider = default;
     [SerializeField] Animator buttonAnimator = default;
