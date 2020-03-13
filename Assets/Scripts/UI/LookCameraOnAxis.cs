d7216afb6f3589a3019d4dda4e6a7947c8244e7a
PLSAM <samarachtingi@gmail.com>
2020-03-13 14:11:20 +0100

    img objectif

---
+++ Assets/Scripts/UI/LookCameraOnAxis.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookCameraOnAxis : MonoBehaviour
+{
+    [SerializeField] Vector3 rota;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform.position);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x * rota.x, transform.eulerAngles.y * rota.y, transform.eulerAngles.z * rota.z);
+    }
+}
