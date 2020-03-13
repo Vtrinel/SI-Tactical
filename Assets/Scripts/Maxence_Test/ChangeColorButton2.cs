fd63a547d211cde9ade1901754e157c0563726d9
Dulours <SeldoonK@gmail.com>
2020-03-13 16:46:58 +0100

    Menu GA

---
+++ Assets/Scripts/Maxence_Test/ChangeColorButton2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ChangeColorButton2 : MonoBehaviour
+{
+    public Color32 ColorOutlineAndTest;
+    public GameObject Outline;
+    public GameObject Text;
+
+    public void SetColortoSelection()
+    {
+       
+        Outline.GetComponent<Image>().color = ColorOutlineAndTest;
+        Text.GetComponent<TextMeshPro>().color = ColorOutlineAndTest;
+
+    }
+
+    public void SetColortoExitSelection()
+    {
+      
+        Outline.GetComponent<Image>().color = Color.white;
+        Text.GetComponent<TextMeshPro>().color = Color.white;
+
+    }
+}
