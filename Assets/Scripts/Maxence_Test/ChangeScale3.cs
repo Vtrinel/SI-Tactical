d3852f78ff25710bc88c268f247da550c0ef05a3
Yume <Yumekua@gmail.com>
2020-03-13 17:41:46 +0100

    Anim you lose & you win fait

---
+++ Assets/Scripts/Maxence_Test/ChangeScale3.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class ChangeScale3 : MonoBehaviour
+{
+
+    public Vector3 scaleBase;
+    public Vector3 GrosScale;
+
+    public void SetScaletoSelection()
+    {
+
+        transform.localScale = GrosScale;
+    }
+
+    public void SetScaletoExitSelection()
+    { 
+    
+            transform.localScale = scaleBase;
+        
+    }
+
+}
