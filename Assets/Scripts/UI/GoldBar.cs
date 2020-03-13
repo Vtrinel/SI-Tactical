2367e307c85579f41f2fd3cbfe658152382611ef
Yume <Yumekua@gmail.com>
2020-03-13 16:36:08 +0100

    Arbre de compétence habillage finish

--- Assets/Scripts/UI/GoldBar.cs
+++ Assets/Scripts/UI/GoldBar.cs
@@ -18,6 +18,7 @@ public class GoldBar : MonoBehaviour
 
     float currentGold = 0;
     bool maxGold = false;
+    public bool tooltipsDesactived;
 
     [SerializeField] float timeToFill = 3;
 
@@ -89,11 +90,13 @@ public class GoldBar : MonoBehaviour
 
     public void StartTooltip()
     {
+        if(tooltipsDesactived == false)
         barAnimator.SetBool("Tooltiped", true);
     }
 
     public void EndTooltip()
     {
-        barAnimator.SetBool("Tooltiped", false);
+        if (tooltipsDesactived == false)
+            barAnimator.SetBool("Tooltiped", false);
     }
 }
